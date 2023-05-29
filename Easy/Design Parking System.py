8dd78e4fc2f4a45cb1bf68702174de93ea64aa63
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-05-29 10:04:11 +0100

    Design Parking System

---
+++ Easy/Design Parking System.py
@@ -0,0 +1,22 @@
+# Design Parking System
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.medium = medium
+        self.small = small
+
+    def addCar(self, carType: int) -> bool:
+        if carType == 1:
+            if self.big > 0 :
+                self.big -= 1
+                return True
+        if carType == 2:
+            if self.medium > 0:
+                self.medium -= 1
+                return True
+        if carType == 3:
+            if self.small > 0:
+                self.small -= 1
+                return True
+        return False
