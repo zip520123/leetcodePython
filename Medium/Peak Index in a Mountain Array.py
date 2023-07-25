61232cc57d3e7e89eb520d6ef410eebdd5e22674
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-07-25 11:03:47 +0100

    Peak Index in a Mountain Array

---
+++ Medium/Peak Index in a Mountain Array.py
@@ -0,0 +1,10 @@
+# Peak Index in a Mountain Array
+def peakIndexInMountainArray(self, arr: List[int]) -> int:
+    l,r = 0, len(arr)-1
+    while l<r:
+        mid = l+((r-l)>>1)
+        if arr[mid] < arr[mid+1]:
+            l = mid+1
+        else:
+            r = mid
+    return l
