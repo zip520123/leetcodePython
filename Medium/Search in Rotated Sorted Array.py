dc7d45f59871f3e8d438b7c460a6b81e8ab864ad
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-08-08 10:57:32 +0100

    Search in Rotated Sorted Array

---
+++ Medium/Search in Rotated Sorted Array.py
@@ -0,0 +1,19 @@
+# Search in Rotated Sorted Array
+# O(log n), O(1)
+def search(self, nums: List[int], target: int) -> int:
+    l,r = 0, len(nums)-1
+    while l<=r:
+        mid = l+((r-l)>>1)
+        if nums[mid] == target:
+            return mid
+        if nums[l] <= nums[mid]:
+            if target > nums[mid] or target < nums[l]:
+                l = mid+1
+            else:
+                r = mid-1
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid-1
+            else:
+                l = mid+1
+    return -1
