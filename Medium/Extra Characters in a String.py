7ff5d8a85846f04156ef59c81d731cecc695902e
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-09-02 12:50:50 +0100

    Extra Characters in a String

---
+++ Medium/Extra Characters in a String.py
@@ -0,0 +1,62 @@
+# Extra Characters in a String
+# O(n^2), O(n)
+class Trie:
+    def __init__(self):
+        self.map = {}
+        self.n = None
+class Solution:
+
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        root = Trie()
+        for word in dictionary:
+            curr = root
+            for char in word:
+                if char not in curr.map:
+                    curr.map[char] = Trie()
+                curr = curr.map[char]
+            curr.n = len(word)
+        
+        @cache
+        def dfs(index: int, rest: int) -> int:
+            if index == len(s): return rest
+            res = rest
+            for i in range(index, len(s)):
+                curr = root
+                j = i
+                
+                while j < len(s) and s[j] in curr.map: 
+                    curr = curr.map[s[j]]
+                    j += 1
+                    if curr.n is not None:
+                        res = min(res, dfs(j, rest-curr.n))
+            return res
+        return dfs(0, len(s))
+    
+
+class Solution:
+
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        root = Trie()
+        for word in dictionary:
+            curr = root
+            for char in word:
+                if char not in curr.map:
+                    curr.map[char] = Trie()
+                curr = curr.map[char]
+            curr.isEnd = True
+        
+        @cache
+        def dfs(index: int, rest: Optional[str]) -> int:
+            if index == len(s): return rest
+            res = rest
+            for i in range(index, len(s)):
+                curr = root
+                j = i
+                
+                while j < len(s) and s[j] in curr.map: 
+                    curr = curr.map[s[j]]
+                    j += 1
+                    if curr.isEnd:
+                        res = min(res, dfs(j, rest-(j-i)))
+            return res
+        return dfs(0, len(s))
