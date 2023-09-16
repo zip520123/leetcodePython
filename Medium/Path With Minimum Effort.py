5df15002519274e4946f57a8ad2ef89aa8f35608
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-09-16 09:26:36 +0100

    Path With Minimum Effort

---
+++ Medium/Path With Minimum Effort.py
@@ -0,0 +1,20 @@
+# Path With Minimum Effort
+# O(n log n), O(n)
+def minimumEffortPath(self, heights: List[List[int]]) -> int:
+    rows, cols = len(heights), len(heights[0])
+    memo = defaultdict((lambda: math.inf))
+    memo[(0,0)] = 0
+    minHeap = [(0,0,0)]
+
+    while minHeap:
+        effort, x, y = heappop(minHeap)
+        if x == rows-1 and y == cols-1: return effort
+
+        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            nx, ny = x+dx, y+dy
+
+            if 0 <= nx < rows and 0 <= ny < cols:
+                neffort = max(effort, abs(heights[x][y]-heights[nx][ny]))
+                if neffort < memo[(nx,ny)]:
+                    memo[(nx,ny)] = neffort
+                    heappush(minHeap, (neffort, nx,ny))
