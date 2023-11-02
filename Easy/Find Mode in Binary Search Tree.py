1f7a544316b5682286095e370a79e03d4682bfbf
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-11-02 22:07:58 +0900

    Find Mode in Binary Search Tree

--- Easy/Find Mode in Binary Search Tree.py
+++ Easy/Find Mode in Binary Search Tree.py
@@ -17,3 +17,28 @@ def findMode(self, root: Optional[TreeNode]) -> List[int]:
         elif val == count:
             res.append(key)
     return res
+
+# O(n), O(1)
+def findMode(self, root: Optional[TreeNode]) -> List[int]:
+    count, maxCount = 0, -1
+    res = []
+    prev = None
+    def dfs(root: Optional[TreeNode]):
+        nonlocal count, maxCount, res, prev
+        if root != None:
+            dfs(root.left)
+            if root.val == prev:
+                count += 1
+            else:
+                count = 1
+            if count > maxCount:
+                res = [root.val]
+                maxCount = count
+            elif count == maxCount:
+                res.append(root.val)
+            prev = root.val
+            
+            dfs(root.right)
+    dfs(root)
+
+    return res
