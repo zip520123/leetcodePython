90f5d2eb71fc137dc974778843a69c8c0bb274c5
Hsiang-Lin <zip520123@yahoo.com.tw>
2023-11-13 20:48:19 +0900

    Bus Routes improve readability

--- Hard/Bus Routes.py
+++ Hard/Bus Routes.py
@@ -20,9 +20,9 @@ def numBusesToDestination(self, routes: List[List[int]], source: int, target: in
         for routeIndex in temp:
             for stop in routes[routeIndex]:
                 if stop == target: return res
-                for route in graph[stop]:
-                    if route not in seen:
-                        seen.add(route)
-                        queue.append(route)
+                for nextRouteIndex in graph[stop]:
+                    if nextRouteIndex not in seen:
+                        seen.add(nextRouteIndex)
+                        queue.append(nextRouteIndex)
         
     return -1
