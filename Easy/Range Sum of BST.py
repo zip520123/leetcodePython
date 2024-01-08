4d23909fb75770e1f69035917bebcd2530d712de
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-01-08 10:40:39 +0000

    Range Sum of BST

--- Easy/Range Sum of BST.py
+++ Easy/Range Sum of BST.py
@@ -6,3 +6,18 @@ def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
     if low <= root.val <= high:
         res += root.val
     return res + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+# O(n), O(w)
+def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+    if root == None: return 0
+    q = [root]
+    res = 0
+    while q:
+        temp = q
+        q = []
+        for node in temp:
+            if low <= node.val <= high:
+                res += node.val
+            if node.left != None: q.append(node.left)
+            if node.right != None: q.append(node.right)
+    return res
