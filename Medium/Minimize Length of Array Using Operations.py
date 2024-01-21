63632a7a569994c28044aa5a1ef9cfaebf41fdc4
zip520123 <zip520123@yahoo.com.tw>
2024-01-21 00:26:07 +0000

    Minimize Length of Array Using Operations

--- Medium/Minimize Length of Array Using Operations.py
+++ Medium/Minimize Length of Array Using Operations.py
@@ -1,11 +1,7 @@
 # Minimize Length of Array Using Operations
 # O(n log n), O(n)
 def minimumArrayLength(self, nums: List[int]) -> int:
-    arr = sorted(nums)
-    n = len(nums)
-    min_num = arr[0]
-    for curr in nums[1:]:
-        if curr % min_num != 0:
-            return 1
-
+    min_num = min(nums)
+    for n in nums:
+        if n % min_num != 0: return 1
     return (nums.count(min_num) + 1) // 2
