c99fa3fed1f7a19c5799bc1b5657b4b3b11aff66
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-02-01 16:30:52 +0000

    Maximum Subarray

--- Medium/Maximum Subarray.py
+++ Medium/Maximum Subarray.py
@@ -7,3 +7,14 @@ def maxSubArray(self, nums: List[int]) -> int:
         dp = max(dp+nums[i], nums[i])
         res = max(res, dp)
     return res
+
+# O(n), O(1)
+def maxSubArray(self, nums: List[int]) -> int:
+    res = nums[0]
+    curr = nums[0]
+    for i in range(1, len(nums)):
+        n = nums[i]
+        if curr < 0: curr = 0
+        curr += n
+        res = max(res, curr)
+    return res
