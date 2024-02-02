ece27efa753d5ca87b5cb6639437f494d65bacad
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-02-02 16:00:04 +0000

    Sequential Digits

--- Medium/Sequential Digits.py
+++ Medium/Sequential Digits.py
@@ -13,3 +13,14 @@ def sequentialDigits(self, low: int, high: int) -> List[int]:
             if digit == 10:
                 break
     return sorted(res)
+
+# O(n), O(n)
+def sequentialDigits(self, low: int, high: int) -> List[int]:
+    res = []
+    arr = [str(i) for i in range(1,10)]
+    for digits in range(2,10):
+        for i in range(10-digits):
+            curr = int("".join(arr[i:i+digits]))
+            if low <= curr <= high:
+                res.append(curr)
+    return res
