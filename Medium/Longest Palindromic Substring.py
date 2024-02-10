179eb98c015ff2dd23e1ee846866926f0b26e158
zip520123 <zip520123@yahoo.com.tw>
2024-02-10 09:20:41 +0000

    Longest Palindromic Substring

--- Medium/Longest Palindromic Substring.py
+++ Medium/Longest Palindromic Substring.py
@@ -23,3 +23,28 @@ def longestPalindrome(self, s: str) -> str:
             length = r-l+1
             index = l
     return s[index: index+length]
+
+# O(n^2), O(1)
+def longestPalindrome(self, s: str) -> str:
+    n = len(s)
+    index = 0
+    length = 1
+    for i in range(n):
+        l, r = i, i
+        while l-1>=0 and r+1<n and s[l-1] == s[r+1]:
+            l -= 1
+            r += 1
+        if length < r-l+1:
+            length = r-l+1
+            index = l
+
+        if i + 1 <n and s[i+1] == s[i]:
+            l, r = i, i + 1
+            while l-1>=0 and r+1<n and s[l-1] == s[r+1]:
+                l -= 1
+                r += 1
+            if length < r-l+1:
+                length = r-l+1
+                index = l
+        
+    return s[index: index+length]
