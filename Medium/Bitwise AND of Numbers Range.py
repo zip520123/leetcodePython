c705815f56ac490ada3facb90cc250f099f9a726
zip520123 <zip520123@yahoo.com.tw>
2024-02-21 19:16:32 +0000

    Bitwise AND of Numbers Range

--- Medium/Bitwise AND of Numbers Range.py
+++ Medium/Bitwise AND of Numbers Range.py
@@ -6,3 +6,12 @@ def rangeBitwiseAnd(self, left: int, right: int) -> int:
         if right - left < 2 ** i and (left & 2 ** i) and (right & 2 ** i):
             res += 2 ** i
     return res
+
+# O(1), O(1)
+def rangeBitwiseAnd(self, left: int, right: int) -> int:
+    count = 0
+    while left != right:
+        left >>= 1
+        right >>= 1
+        count += 1
+    return left << count
