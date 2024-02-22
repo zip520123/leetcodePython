46bbbf809366ea6bf0b9ca4ac1a698f743b70292
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-02-22 11:12:38 +0000

    Find the Town Judge

--- Easy/Find the Town Judge.py
+++ Easy/Find the Town Judge.py
@@ -15,3 +15,20 @@ def findJudge(self, n: int, trust: List[List[int]]) -> int:
     if res == None:
         return -1
     return res
+
+# O(n), O(n)
+def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    memo1 = defaultdict(int)
+    memo2 = defaultdict(int)
+    for a,b in trust:
+        memo1[a] += 1
+        memo2[b] += 1
+    res = None
+    for p in range(1,n+1):
+        if memo1[p] == 0 and memo2[p] == n-1:
+            if res != None:
+                return -1
+            res = p
+    if res == None:
+        return -1
+    return res
