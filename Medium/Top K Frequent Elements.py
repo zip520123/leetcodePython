a692efff71fb3892ce17246c8934f2a5a013217c
zip520123 <zip520123@yahoo.com.tw>
2024-02-22 21:58:48 +0000

    Top K Frequent Elements

--- Medium/Top K Frequent Elements.py
+++ Medium/Top K Frequent Elements.py
@@ -6,3 +6,18 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dict[n] += 1
     arr = [key for _,key in enumerate(dict)]
     return sorted(arr, key=lambda key: dict[key], reverse = True)[0:k]
+
+# O(n log k), O(n)
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    memo = defaultdict(int)
+    for n in nums:
+        memo[n] += 1
+    heap = []
+    for key, val in memo.items():
+        heapq.heappush(heap, (val, key))
+        if len(heap) > k:
+            heapq.heappop(heap)
+    res = []
+    for val, key in heap:
+        res.append(key)
+    return res
