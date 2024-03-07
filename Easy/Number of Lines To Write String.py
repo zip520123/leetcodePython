85c154ad15b3145f95cf07544c3ac3c3583571b3
zip520123 <zip520123@yahoo.com.tw>
2024-03-07 08:52:58 +0000

    Number of Lines To Write String

--- Easy/Number of Lines To Write String.py
+++ Easy/Number of Lines To Write String.py
@@ -2,14 +2,11 @@
 # O(n), O(1)
 def numberOfLines(self, widths: List[int], s: str) -> List[int]:
     currWidths = 0
-    currCount = 0
     lines = 1
     for char in s:
         width = widths[ord(char) - ord("a")]
         if currWidths + width > 100:
             currWidths = 0
-            currCount = 0
             lines += 1
         currWidths += width
-        currCount += 1
     return [lines, currWidths]
