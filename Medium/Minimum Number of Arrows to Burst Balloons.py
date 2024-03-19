c24e0666b06e0c883c694c724ef1fd19b59295d6
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-03-19 15:15:42 +0000

    Minimum Number of Arrows to Burst Balloons

--- Medium/Minimum Number of Arrows to Burst Balloons.py
+++ Medium/Minimum Number of Arrows to Burst Balloons.py
@@ -11,3 +11,16 @@ def findMinArrowShots(self, points: List[List[int]]) -> int:
         else:
             curr = arr[i][1]
     return res
+
+# O(n log n), O(n)
+def findMinArrowShots(self, points: List[List[int]]) -> int:
+    arr = sorted(points)
+    res = 0
+    curr = -math.inf
+    for i in range(len(arr)):
+        if arr[i][0] > curr:
+            res += 1
+            curr = max(curr, arr[i][1])
+        else:
+            curr = min(curr, arr[i][1])
+    return res
