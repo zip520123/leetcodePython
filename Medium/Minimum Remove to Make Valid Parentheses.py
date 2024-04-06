d4174d8d4caf8117365bedf305f670dbf5b275a4
zip520123 <zip520123@yahoo.com.tw>
2024-04-06 16:04:40 +0100

    Minimum Remove to Make Valid Parentheses

--- Medium/Minimum Remove to Make Valid Parentheses.py
+++ Medium/Minimum Remove to Make Valid Parentheses.py
@@ -19,3 +19,25 @@ def minRemoveToMakeValid(self, s: str) -> str:
         if i not in incorrect:
             res += s[i]
     return res
+
+# O(n), O(n)
+def minRemoveToMakeValid(self, s: str) -> str:
+    stack = []
+    for i in range(len(s)):
+        char = s[i]
+        if char == ")":
+            if stack and stack[-1][0] == "(":
+                stack.pop(-1)
+            else:
+                stack.append((char, i))
+        elif char == "(":
+            stack.append((char, i))
+    
+    res = ""
+    for i in range(len(s)):
+        char = s[i]
+        if stack and stack[0][1] == i:
+            stack.pop(0)
+        else:
+            res += char
+    return res
