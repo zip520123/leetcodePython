0eac78475264fee31f1573fd8b58366e329b96eb
Hsiang-Lin <zip520123@yahoo.com.tw>
2024-06-11 12:05:06 +0100

    Relative Sort Array

--- Easy/Relative Sort Array.py
+++ Easy/Relative Sort Array.py
@@ -16,3 +16,20 @@ def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
     res = sorted(res1, key = lambda x: memo[x]) + sorted(res2)
 
     return res
+
+# O(n), O(n), Counting Sort
+def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+    maxN = max(arr1)
+    count = [0] * (maxN + 1)
+    for n in arr1:
+        count[n] += 1
+    res = []
+    for n in arr2:
+        while count[n] > 0:
+            res.append(n)
+            count[n] -= 1
+    for i in range(len(count)):
+        while count[i] > 0:
+            res.append(i)
+            count[i] -= 1
+    return res
