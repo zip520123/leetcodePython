b9f84c624896faf5e61111ed40d3290f9d6f8da8
zip520123 <zip520123@yahoo.com.tw>
2024-06-12 09:57:34 +0100

    Sort Colors

--- Medium/Sort Colors.py
+++ Medium/Sort Colors.py
@@ -14,3 +14,16 @@ def sortColors(self, nums: List[int]) -> None:
     
     for i in range(len(res)):
         nums[i] = res[i]
+# O(n), O(1)
+def sortColors(self, nums: List[int]) -> None:
+    i, l, r = 0, 0, len(nums)-1
+    while i <= r:
+        if nums[i] == 0:
+            nums[i], nums[l] = nums[l], nums[i]
+            i += 1
+            l += 1
+        elif nums[i] == 1:
+            i += 1
+        else:
+            nums[i], nums[r] = nums[r], nums[i]
+            r -= 1
