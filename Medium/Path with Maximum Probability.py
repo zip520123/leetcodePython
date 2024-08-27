8342ad04569cdaed3ac9d356d86ef4fef0f4ebf5
zip520123 <zip520123@yahoo.com.tw>
2024-08-27 09:57:30 +0100

    Path with Maximum Probability

--- Medium/Path with Maximum Probability.py
+++ Medium/Path with Maximum Probability.py
@@ -18,3 +18,27 @@ def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], 
                 arr[nextNode] = -nextProb*prob
                 heapq.heappush(pq, (nextProb*prob, nextNode))
     return 0
+
+def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+    graph = defaultdict(list)
+    
+    for i in range(len(edges)):
+        node1, node2 = edges[i][0], edges[i][1]
+        p = succProb[i]
+        graph[node1].append((node2, p))
+        graph[node2].append((node1, p)) 
+
+    heap = [(-1, start_node)]
+
+    arr = [0] * n
+    arr[start_node] = 1
+    while heap:
+        p, node = heapq.heappop(heap)
+        p = -p
+        if node == end_node: return p
+        
+        for next_node, next_probability in graph[node]:
+            if arr[next_node] < p * next_probability:
+                arr[next_node] = p * next_probability 
+                heapq.heappush(heap, (-p * next_probability, next_node))
+    return 0
