48d21372d222940d418e70d3bc120c05b4fdfb02
zip520123 <zip520123@yahoo.com.tw>
2024-09-08 09:58:14 +0200

    Split Linked List in Parts

--- Medium/Split Linked List in Parts.py
+++ Medium/Split Linked List in Parts.py
@@ -21,3 +21,28 @@ def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[Li
         if prev:
             prev.next = None
     return res
+
+# O(n), O(1)
+def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+    curr = head
+    count = 0
+    while curr:
+        count += 1
+        curr = curr.next
+    res = []
+    num_of_nodes = count // k
+    remain_nodes = count % k
+    curr = head
+    for i in range(k):
+        root = curr
+        prev = None
+        for _ in range(num_of_nodes):
+            prev = curr
+            curr = curr.next
+        if i < remain_nodes:
+            prev = curr
+            curr = curr.next
+        if prev:
+            prev.next = None
+        res.append(root)
+    return res
