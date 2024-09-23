0c9ffdb07c1fbaee39a31c8c74a359bda4c925e8
zip520123 <zip520123@yahoo.com.tw>
2024-09-23 11:19:08 +0100

    Extra Characters in a String

--- Medium/Extra Characters in a String.py
+++ Medium/Extra Characters in a String.py
@@ -34,29 +34,29 @@ class Solution:
     
 
 class Solution:
-
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         root = Trie()
         for word in dictionary:
             curr = root
             for char in word:
-                if char not in curr.map:
-                    curr.map[char] = Trie()
-                curr = curr.map[char]
+                if char not in curr.memo:
+                    curr.memo[char] = Trie()
+                curr = curr.memo[char]
             curr.isEnd = True
         
         @cache
-        def dfs(index: int, rest: Optional[str]) -> int:
-            if index == len(s): return rest
+        def dfs(index: int, rest: int) -> int:
+            if index == len(s):
+                return rest
             res = rest
             for i in range(index, len(s)):
                 curr = root
                 j = i
-                
-                while j < len(s) and s[j] in curr.map: 
-                    curr = curr.map[s[j]]
+                while j<len(s) and s[j] in curr.memo: 
+                    curr = curr.memo[s[j]]
                     j += 1
                     if curr.isEnd:
                         res = min(res, dfs(j, rest-(j-i)))
             return res
+
         return dfs(0, len(s))
