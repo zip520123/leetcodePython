b911609f2d774c55b4d6fd64ac721cc3f59174b7
zip520123 <zip520123@yahoo.com.tw>
2024-10-19 10:26:08 +0200

    Find Kth Bit in Nth Binary String

---
+++ Medium/Find Kth Bit in Nth Binary String.py
@@ -0,0 +1,8 @@
+# Find Kth Bit in Nth Binary String
+# O(n), O(n)
+def findKthBit(self, n: int, k: int) -> str:
+    res = [0]
+    for _ in range(n-1):
+        temp = res
+        res = res + [1] + list(map(lambda x: 0 if x == 1 else 1, reversed(temp)))
+    return str(res[k-1])
