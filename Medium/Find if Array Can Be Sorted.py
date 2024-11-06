61573dbbd0a49a414aa13bc11d10fb304185f343
zip520123 <zip520123@yahoo.com.tw>
2024-11-06 18:59:56 +0000

    Find if Array Can Be Sorted

--- Medium/Find if Array Can Be Sorted.py
+++ Medium/Find if Array Can Be Sorted.py
@@ -18,3 +18,32 @@ def canSortArray(self, nums: List[int]) -> bool:
         sortArr += sorted(arr)
     
     return sortArr == sorted(nums)
+
+# O(n), O(n)
+def canSortArray(self, nums: List[int]) -> bool:
+    first_ones = 0
+    first = nums[0]
+    while first:
+        first_ones += first % 2
+        first >>= 1
+    
+    segments = [[]]
+
+    for n in nums:
+        ones = 0
+        curr = n
+        while curr:
+            ones += curr % 2
+            curr >>= 1
+        if ones == first_ones:
+            segments[-1].append(n)
+        else:
+            segments.append([n])
+            first_ones = ones
+    
+    for i in range(1, len(segments)):
+        curr_segment = segments[i]
+        prev_segment = segments[i-1]
+        if max(prev_segment) > min(curr_segment):
+            return False
+    return True
