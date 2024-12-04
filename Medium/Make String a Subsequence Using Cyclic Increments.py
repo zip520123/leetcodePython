796fb0389c5c6f417f027373483e0303a93505e0
zip520123 <zip520123@yahoo.com.tw>
2024-12-04 12:10:00 +0000

    Make String a Subsequence Using Cyclic Increments

---
+++ Medium/Make String a Subsequence Using Cyclic Increments.py
@@ -0,0 +1,16 @@
+# Make String a Subsequence Using Cyclic Increments
+# O(n), O(1)
+def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+    cyclically = {}
+    for asci_num in range(ord("a"), ord("z")+1):
+        char = chr(asci_num)
+        next_char = chr(((asci_num + 1 - ord("a")) % 26) + ord("a"))
+        cyclically[char] = next_char
+    l, r = 0, 0
+    while l < len(str1) and r < len(str2):
+        if str1[l] == str2[r] or cyclically[str1[l]] == str2[r]:
+            r += 1
+        l += 1
+    if r == len(str2):
+        return True
+    return False
