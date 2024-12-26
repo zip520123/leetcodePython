bda016ec274556543bf1a2ddc17483f45c22b3b2
zip520123 <zip520123@yahoo.com.tw>
2024-12-27 07:29:58 +0800

    Target Sum

--- Medium/Target Sum.py
+++ Medium/Target Sum.py
@@ -1,5 +1,5 @@
 # Target Sum
-# O(2^n) time complexity, O(n) space complexity
+# O(2^n) time complexity, O(n) space complexity, TLE
 def findTargetSumWays(self, nums: List[int], target: int) -> int:
     def dfs(index, count) -> int:
         if index == len(nums):
@@ -7,5 +7,32 @@ def findTargetSumWays(self, nums: List[int], target: int) -> int:
                 return 1
             return 0
         return dfs(index+1, count + nums[index]) + dfs(index+1, count - nums[index])
+    
+    return dfs(0, 0)
+
+# O(n*sum) time complexity, O(n*sum) space complexity
+def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    @cache
+    def dfs(index, count) -> int:
+        if index == len(nums):
+            if count == target:
+                return 1
+            return 0
+        return dfs(index+1, count + nums[index]) + dfs(index+1, count - nums[index])
+    
+    return dfs(0, 0)
+
+# O(n*sum) time complexity, O(n*sum) space complexity
+def findTargetSumWays(self, nums: List[int], target: int) -> int:
+    memo = {}
+    def dfs(index, count) -> int:
+        if (index, count) in memo:
+            return memo[(index, count)]
+        if index == len(nums):
+            if count == target:
+                return 1
+            return 0
+        memo[(index, count)] = dfs(index+1, count + nums[index]) + dfs(index+1, count - nums[index])
+        return memo[(index,count)]
     
     return dfs(0, 0)
