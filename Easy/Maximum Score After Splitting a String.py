3eb0642a8349f4c9169f55c2f85b75a49fe77cc0
zip520123 <zip520123@yahoo.com.tw>
2025-01-01 08:26:28 +0800

    Maximum Score After Splitting a String

--- Easy/Maximum Score After Splitting a String.py
+++ Easy/Maximum Score After Splitting a String.py
@@ -14,3 +14,23 @@ def maxScore(self, s: str) -> int:
         if char == "1": ones -= 1
         res = max(res, zeros + ones)
     return res
+
+# O(n), O(1)
+def maxScore(self, s: str) -> int:
+    res = 0
+    zeros, ones = 0, 0
+
+    if s[0] == "0":
+        zeros += 1
+    
+    for i in range(1, len(s)):
+        if s[i] == "1":
+            ones += 1
+
+    for i in range(1, len(s)):
+        res = max(res, zeros + ones)
+        if s[i] == "0":
+            zeros += 1
+        else:
+            ones -= 1
+    return res
