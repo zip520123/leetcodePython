1e79953592e8e10af63a3ecd8d38389604099811
zip520123 <zip520123@yahoo.com.tw>
2025-01-02 08:46:31 +0800

    Count Special Subsequences

--- Medium/Count Special Subsequences.py
+++ Medium/Count Special Subsequences.py
@@ -10,3 +10,17 @@ def numberOfSubsequences(self, nums: List[int]) -> int:
                         res += 1
     return res
 
+# Count Special Subsequences
+# O(n^2), O(n)
+def numberOfSubsequences(self, nums: List[int]) -> int:
+    res = 0
+    memo = defaultdict(int)
+    for r in range(4, len(nums)-2):
+        q = r - 2
+        for p in range(q-1):
+            key = nums[p]/nums[q]
+            memo[key] += 1
+        for s in range(r + 2, len(nums)):
+            res += memo[nums[s]/nums[r]]
+
+    return res
