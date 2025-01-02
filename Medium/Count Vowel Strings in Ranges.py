224c264ff53575aa6586c21019fdf214942fabd4
zip520123 <zip520123@yahoo.com.tw>
2025-01-02 08:25:02 +0800

    Count Vowel Strings in Ranges

--- Medium/Count Vowel Strings in Ranges.py
+++ Medium/Count Vowel Strings in Ranges.py
@@ -10,3 +10,12 @@ def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
     for start, end in queries:
         res.append(prefix[end+1]-prefix[start] )
     return res
+
+def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+    prefix = [0] * (len(words) + 1)
+    for i in range(1, len(words) + 1):
+        prefix[i] = prefix[i-1]
+        if words[i-1][0] in "aeiou" and words[i-1][-1] in "aeiou":
+            prefix[i] += 1
+    
+    return [ prefix[end+1]-prefix[start] for start, end in queries]
