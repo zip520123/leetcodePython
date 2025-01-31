eae93a4aedcc0fae55e39a009c0e4d005b81d1a4
zip520123 <zip520123@yahoo.com.tw>
2025-01-31 19:32:28 +0800

    Making A Large Island

--- Hard/Making A Large Island.py
+++ Hard/Making A Large Island.py
@@ -63,3 +63,59 @@ def largestIsland(self, grid: List[List[int]]) -> int:
                     curr += rootToCount[root]
                 res = max(res, curr)
     return res
+
+# 
+def largestIsland(self, grid: List[List[int]]) -> int:
+    memo = {}
+    def find(node):
+        if node not in memo:
+            memo[node] = node
+        if memo[node] != node:
+            memo[node] = find(memo[node])
+        return memo[node]
+    def union(node1, node2):
+        root1, root2 = find(node1), find(node2)
+        memo[root1] = root2
+    
+    rows, cols = len(grid), len(grid[0])
+    dp = [[0]* cols for _ in range(rows) ]
+    for row in range(rows):
+        for col in range(cols):
+            dp[row][col] = grid[row][col]
+    res = 0
+    roots_count = {}
+    for row in range(rows):
+        for col in range(cols):
+            if dp[row][col] == 1:
+                queue = [(row, col)]
+                seen = set()
+                
+                while queue:
+                    temp = queue
+                    queue = []
+                    for curr_row, curr_col in temp:
+                        if (curr_row, curr_col) not in seen:
+                            seen.add((curr_row, curr_col))
+                            dp[curr_row][curr_col] = 0
+                            for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                                next_row, next_col = curr_row + dy, curr_col + dx
+                                if 0 <= next_row < rows and 0 <= next_col < cols and dp[next_row][next_col] == 1:
+                                    queue.append((next_row, next_col))
+
+                for seen_row, seen_col in seen:
+                    union((row, col), (seen_row, seen_col))
+                roots_count[ find((row, col))] = len(seen)
+                res = max(res, len(seen))
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 0:
+                root_set = set()
+                for dx, dy in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                    next_row, next_col = row + dy, col + dx
+                    if 0 <= next_row < rows and 0 <= next_col < cols and grid[next_row][next_col] == 1:
+                        root_set.add(find((next_row, next_col)))
+                curr = 1
+                for root in root_set:
+                    curr += roots_count[root]
+                res = max(res, curr)
+    return res
