8040bff1f71743f595a0c24441c15acd37a0d1bf
zip520123 <zip520123@yahoo.com.tw>
2025-03-04 10:28:11 +0000

    Check if Number is a Sum of Powers of Three

---
+++ Medium/Check if Number is a Sum of Powers of Three.py
@@ -0,0 +1,16 @@
+# Check if Number is a Sum of Powers of Three
+# O(n), O(1)
+def checkPowersOfThree(self, n: int) -> bool:
+    power = 0
+    while 3 ** power <= n:
+        power += 1
+    while n > 0:
+        if n >= 3 ** power:
+            n -= 3 ** power
+        
+        if n >= 3 ** power:
+            return False
+        
+        power -= 1
+    
+    return True
