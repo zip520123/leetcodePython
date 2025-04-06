45096843565d53fa2a1bd2675d67e5d2fddcafa5
zip520123 <zip520123@gmail.com>
2025-04-06 10:13:56 +0100

    Valid Word Abbreviation

--- Easy/Valid Word Abbreviation.py
+++ Easy/Valid Word Abbreviation.py
@@ -20,3 +20,26 @@ def validWordAbbreviation(self, word: str, abbr: str) -> bool:
             else:
                 return False
     return l == ln and r == rn
+
+# O(word+abbr), O(1)
+def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+    i = 0
+    j = 0
+    while j < len(abbr) and i < len(word):
+        if abbr[j].isdigit():
+            if int(abbr[j]) == 0:
+                return False
+            curr = 0
+            while j < len(abbr) and abbr[j].isdigit():
+                curr *= 10
+                curr += int(abbr[j])
+                j += 1
+            
+            i += curr
+        else:
+            if word[i] != abbr[j]:
+                return False        
+            i += 1
+            j += 1
+
+    return i == len(word) and j == len(abbr)
