d9d77871e6708a74767491ad601cd1fbb0344d7e
zip520123 <zip520123@gmail.com>
2025-04-17 12:53:08 +0100

    Nested List Weight Sum

--- Medium/Nested List Weight Sum.py
+++ Medium/Nested List Weight Sum.py
@@ -9,3 +9,15 @@ def depthSum(self, nestedList: List[NestedInteger]) -> int:
             res += dfs(subNode, depth+1)
         return res
     return sum([dfs(node, 1) for node in nestedList])
+
+def depthSum(self, nestedList: List[NestedInteger]) -> int:
+    def dfs(nodes: List[NestedInteger], depth: int = 1) -> int:
+        res = 0
+        for node in nodes:
+            if node.isInteger():
+                res += node.getInteger() * depth
+            else:
+                res += dfs(node.getList(), depth + 1)
+        return res
+
+    return dfs(nestedList)
