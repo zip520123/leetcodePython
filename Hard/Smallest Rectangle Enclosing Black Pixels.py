601da18a154475520afbd94f8163a39be75151bb
zip520123 <zip520123@gmail.com>
2025-04-25 11:57:43 +0100

    Smallest Rectangle Enclosing Black Pixels

---
+++ Hard/Smallest Rectangle Enclosing Black Pixels.py
@@ -0,0 +1,14 @@
+# Smallest Rectangle Enclosing Black Pixels
+# O(n), O(1)
+def minArea(self, image: List[List[str]], x: int, y: int) -> int:
+    top, bottom = x, x
+    left, right = y, y
+    rows, cols = len(image), len(image[0])
+    for row in range(rows):
+        for col in range(cols):
+            if image[row][col] == "1":
+                top = min(top, row)
+                bottom = max(bottom, row)
+                left = min(left, col)
+                right = max(right, col)
+    return (right - left + 1) * (bottom - top + 1)
