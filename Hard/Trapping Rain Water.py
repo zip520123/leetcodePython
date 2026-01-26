2bdba5e6fccc53c3c8ee51fe7b3bd57af3a14fb6
zip520123 <zip520123@gmail.com>
2026-01-26 10:06:32 +0200

    Trapping Rain Water

--- Hard/Trapping Rain Water.py
+++ Hard/Trapping Rain Water.py
@@ -12,3 +12,24 @@ def trap(self, height: List[int]) -> int:
                 res += (min(height[stack[-1]], curr) - prev) * (i-stack[-1]-1)
         stack.append(i)
     return res
+
+# O(n), O(1)
+def trap(self, height: List[int]) -> int:
+    leftMax, rightMax = 0, 0
+    l = 0
+    r = len(height)-1
+    res = 0
+    while l<r:
+        if height[l] < height[r]:
+            if leftMax <= height[l]:
+                leftMax = height[l]
+            else:
+                res += leftMax - height[l]
+            l += 1
+        else:
+            if rightMax <= height[r]:
+                rightMax = height[r]
+            else:
+                res += rightMax - height[r]
+            r -= 1
+    return res
