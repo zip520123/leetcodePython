d50a6ef502e520fea9536e120621757836214b12
zip520123 <zip520123@yahoo.com.tw>
2026-06-15 17:45:28 +0800

    Merge Sorted Array

--- Easy/Merge Sorted Array.py
+++ Easy/Merge Sorted Array.py
@@ -18,3 +18,12 @@ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         r += 1
     for i in range(m+n):
         nums1[i] = arr[i]
+
+# O(n log n), O(n)
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    """
+    Do not return anything, modify nums1 in-place instead.
+    """
+    for i in range(n):
+        nums1[i+m] = nums2[i]
+    nums1.sort()
