76e7bcedcab8f48b1d672079ee630e8a0e0385a8
zip520123 <zip520123@yahoo.com.tw>
2026-06-27 16:15:54 +0800

    Min Cost Climbing Stairs

--- Easy/Min Cost Climbing Stairs.py
+++ Easy/Min Cost Climbing Stairs.py
@@ -9,3 +9,13 @@ def minCostClimbingStairs(self, cost: List[int]) -> int:
         dp2 = dp1
         dp1 = t
     return dp1
+
+# O(n), O(1)
+def minCostClimbingStairs(self, cost: List[int]) -> int:
+    m1 = cost[0]
+    m2 = cost[1]
+    for i in range(2, len(cost)):
+        tempM2 = m2
+        m2 = min(m1, m2) + cost[i]
+        m1 = tempM2
+    return min(m1, m2)
