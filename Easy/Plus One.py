3515c3f37d77d6ded2cf51db9c4aea90d29072df
zip520123 <zip520123@yahoo.com.tw>
2026-06-28 21:55:32 +0800

    Plus One

--- Easy/Plus One.py
+++ Easy/Plus One.py
@@ -16,3 +16,18 @@ def plusOne(self, digits: List[int]) -> List[int]:
     if res[0] == 0:
         res.pop(0)
     return res
+
+# O(n), O(1)
+def plusOne(self, digits: List[int]) -> List[int]:
+    carry = False
+    digits[-1] += 1
+    for i in range(len(digits)-1, -1, -1):
+        if carry:
+            digits[i] += 1
+            carry = False
+        if digits[i] == 10:
+            digits[i] = 0
+            carry = True
+    if carry == True:
+        return [1] + digits
+    return digits
