b3f6e38a4f5ef87a955e3c41217dfbfca4389f06
zip520123 <zip520123@yahoo.com.tw>
2026-07-07 22:48:10 +0800

    Set Matrix Zeroes

--- Medium/Set Matrix Zeroes.py
+++ Medium/Set Matrix Zeroes.py
@@ -16,3 +16,38 @@ def setZeroes(self, matrix: List[List[int]]) -> None:
         for col in range(cols):
             if row in row_arr or col in col_arr:
                 matrix[row][col] = 0
+
+# O(m*n), O(1)
+def setZeroes(self, matrix: List[List[int]]) -> None:
+    first_row_has_zero = False
+    first_col_has_zero = False
+    rows, cols = len(matrix), len(matrix[0])
+    for row in range(rows):
+        if matrix[row][0] == 0:
+            first_col_has_zero = True
+    for col in range(cols):
+        if matrix[0][col] == 0:
+            first_row_has_zero = True
+    
+    for row in range(rows):
+        for col in range(cols):
+            if matrix[row][col] == 0:
+                matrix[row][0] = 0
+                matrix[0][col] = 0
+    for row in range(1, rows):
+        if matrix[row][0] == 0:
+            for col in range(1, cols):
+                matrix[row][col] = 0
+    
+    for col in range(1, cols):
+        if matrix[0][col] == 0:
+            for row in range(1, rows):
+                matrix[row][col] = 0
+    
+    if first_col_has_zero:
+        for row in range(rows):
+            matrix[row][0] = 0
+    
+    if first_row_has_zero:
+        for col in range(cols):
+            matrix[0][col] = 0
