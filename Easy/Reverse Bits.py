181a06caaf67dbff02ec5263110b548ee7636b2a
zip520123 <zip520123@yahoo.com.tw>
2026-07-07 17:35:38 +0800

    Reverse Bits

--- Easy/Reverse Bits.py
+++ Easy/Reverse Bits.py
@@ -7,3 +7,13 @@ def reverseBits(self, n: int) -> int:
         res += n&1
         n >>= 1
     return res
+
+# O(1), O(1)
+def reverseBits(self, n: int) -> int:
+    res = 0
+    for mask in range(32):
+        curr = 2 ** mask
+        bit = n & curr
+        if bit:
+            res += 2 ** (31-mask)
+    return res
