e93a9bb6b05e28e21233069d70bde5f862db3537
zip520123 <zip520123@yahoo.com.tw>
2026-07-20 22:05:32 +0800

    Valid Anagram

--- Easy/Valid Anagram.py
+++ Easy/Valid Anagram.py
@@ -9,3 +9,16 @@ def isAnagram(self, s: str, t: str) -> bool:
         memo[char] -= 1
         if memo[char] < 0: return False
     return True
+
+# O(n), O(n)
+def isAnagram(self, s: str, t: str) -> bool:
+    arr1 = [0] * 26
+    arr2 = [0] * 26
+    for char in s:
+        arr1[ord(char)-ord("a")] += 1
+    for char in t:
+        arr2[ord(char)-ord("a")] += 1
+    for i in range(26):
+        if arr1 != arr2:
+            return False
+    return True
