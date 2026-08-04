0a93e86a6aecdf46bb3275feb8466f7407d49cd9
zip520123 <zip520123@yahoo.com.tw>
2026-08-04 21:06:10 +0800

    Equal Row and Column Pairs

--- Medium/Equal Row and Column Pairs.py
+++ Medium/Equal Row and Column Pairs.py
@@ -1,48 +1,51 @@
 # Equal Row and Column Pairs
-# O(rows*cols*rows), O(row+col)
+# O(rows*cols*n), O(1)
 def equalPairs(self, grid: List[List[int]]) -> int:
+    n = len(grid)
+    rows, cols = n, n
     res = 0
-    rows = len(grid); cols = len(grid[0])
-    for i in range(rows):
-        arr1 = grid[i]
+    
+    for row in range(rows):
         for col in range(cols):
-            arr2 = []
-            for j in range(rows):
-                arr2.append(grid[j][col])
-            if arr1 == arr2:
-                res += 1
+            same = True
+            for i in range(n):
+                if grid[row][i] != grid[i][col]:
+                    same = False
+            if same:
+                res += 1                
     return res
 
-# O(rows*cols*rows), O(1)
+# O(rows*cols*n), O(rows*cols)
 def equalPairs(self, grid: List[List[int]]) -> int:
-    res = 0
     n = len(grid)
-    for i in range(n):
-        arr1 = grid[i]
-        for col in range(n):
-            for row in range(n):
-                n2 = grid[row][col]
-                n1 = arr1[row]
-                if n1 != n2:
-                    break
-            else:
-                res += 1
-    return res
+    rows, cols = n, n
+    res = 0
+    row_map = {}
+    col_map = {}
 
-# O(rows*cols), O(cols)
-def equalPairs(self, grid: List[List[int]]) -> int:
-    res = 0
-    n = len(grid)
-    colsArr = collections.defaultdict(int)
-    for col in range(n):
+    for row in range(rows):
+        row_map[row] = grid[row]
+    for col in range(cols):
         arr = []
-        for row in range(n):
+        for row in range(rows):
             arr.append(grid[row][col])
-        colsArr[tuple(arr)] += 1
+        col_map[col] = arr
+    
+    for row in range(rows):
+        for col in range(cols):
+            if row_map[row] == col_map[col]:
+                res += 1
+    return res
 
-    for i in range(n):
-        arr = grid[i]
-        if tuple(arr) in colsArr:
-            res += colsArr[tuple(arr)]
-                
+# O(rows*cols), O(rows*cols)
+def equalPairs(self, grid: List[List[int]]) -> int:
+    n = len(grid)
+    rows, cols = n, n
+    res = 0
+    row_count = Counter(tuple(row) for row in grid)
+    for col in range(cols):
+        arr = []
+        for row in range(rows):
+            arr.append(grid[row][col])
+        res += row_count[tuple(arr)]
     return res
