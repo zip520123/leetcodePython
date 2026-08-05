48c9ed8c75228c4c8b8693f89359bf89276098ff
zip520123 <zip520123@yahoo.com.tw>
2026-08-05 22:35:37 +0800

    Letter Combinations of a Phone Number

--- Medium/Letter Combinations of a Phone Number.py
+++ Medium/Letter Combinations of a Phone Number.py
@@ -1,5 +1,5 @@
 # Letter Combinations of a Phone Number
-# O(n!), O(n!)
+# O(4^n), O(4^n)
 def letterCombinations(self, digits: str) -> List[str]:
     if digits == "": return []
     map = {
@@ -45,3 +45,27 @@ def letterCombinations(self, digits: str) -> List[str]:
                     queue.append(node + next_char)
 
     return queue
+
+# O(4^n), O(4^n)
+def letterCombinations(self, digits: str) -> List[str]:
+    
+    memo = {2 : "abc",
+    3: "def",
+    4: "ghi",
+    5:"jkl",
+    6:"mno",
+    7:"pqrs",
+    8:"tuv",
+    9:"wxyz"}
+
+    res = []
+
+    def dfs(index: int, path: str):
+        if index == len(digits):
+            res.append(path)
+            return 
+        for char in memo[int(digits[index])]:
+            dfs(index+1, path+char)
+    dfs(0, "")
+
+    return res
