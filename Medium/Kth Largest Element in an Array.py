81ad5c3c638b4aec2fe19f29605cd60ccad730dc
zip520123 <zip520123@yahoo.com.tw>
2026-08-17 19:45:53 +0800

    Kth Largest Element in an Array

--- Medium/Kth Largest Element in an Array.py
+++ Medium/Kth Largest Element in an Array.py
@@ -1,20 +1,25 @@
 # Kth Largest Element in an Array
-# O(n^2)~O(n), O(n)
+# O(n), O(1)
 def findKthLargest(self, nums: List[int], k: int) -> int:
-    n = len(nums)
-    def dfs(left: int, right: int) -> int:
-        l, r, p = left, left, right
-        while r<p:
-            if nums[r] >= nums[p]:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-            r += 1
-        nums[l], nums[p] = nums[p], nums[l]
-        if l == k-1:
-            return nums[l]
-        elif l > k-1:
-            return dfs(0,l-1)
-        else :
-            return dfs(l+1, r)
-    
-    return dfs(0,n-1)
+        n = len(nums)
+        t = n-k
+        left, right = 0, n-1
+        while True:
+            i, l, r = left, left, right
+            p = nums[randint(l,r)]
+            while i<=r:
+                if nums[i] < p:
+                    nums[l], nums[i] = nums[i], nums[l]
+                    l += 1
+                    i += 1
+                elif nums[i] > p:
+                    nums[r], nums[i] = nums[i], nums[r]
+                    r -= 1
+                else:
+                    i += 1
+            if t < l:
+                right = l - 1
+            elif t > r:
+                left = r + 1
+            else:
+                return p
